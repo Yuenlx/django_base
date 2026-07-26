5f0880659ab5937ebde2a70af390b03cca4a9d3a
Yuenlx <15316731929@163.com>
2026-07-26 20:21:49 +0800

    admin站点配置

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class BookInfo(models.Model):
   name = models.CharField(max_length=100)
 
+  def __str__(self):
+    return self.name
+
 class PeopleInfo(models.Model):
   name = models.CharField(max_length=100)
   gender = models.BooleanField()
--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&9pnzx!(kye%v@=lss0j#8ejvgqj0j*h7+nzfxtqc5vnqba-o+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.3']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
